9df6d0715bab432843a14c72700595c95aac89b0
Rishi Kamble <rk23thebest@gmail.com>
2025-10-04 12:21:19 +0530

    Final commit

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-const images = document.querySelectorAll('.images img');
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach(entry =>{
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-            entry.target.classList.remove('hide');
-            
-        } else {
-            entry.target.classList.add('hide');
-            entry.target.classList.remove('show');
-        }
-    })
-}, {threshold:0.5});
-
-images.forEach(image => observer.observe(image));
-
-
 const spanOne = document.querySelector(".one")
 const spanTwo = document.querySelector(".two")
 const spanThree = document.querySelector(".three")
